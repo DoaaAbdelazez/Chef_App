e7296839e5df8b829bad2a813e4655fe42371b7b
Doaa Abdelazez <abdelazezdoaa30@gmail.com>
2023-08-24 03:14:43 +0300

    app text style

---
+++ lib/core/app_text_style.dart
@@ -0,0 +1,36 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle _textStyle({
+  required Color color,
+  required double fonSize,
+  required FontWeight fontWeight,
+}) {
+  return GoogleFonts.lato(
+    color: color,
+    fontSize: fonSize,
+    fontWeight: fontWeight,
+  );
+}
+//bold style
+TextStyle boldStyle({
+  Color color = AppColors.white,
+  double fonSize = 24,
+}) =>
+    _textStyle(
+      color: color,
+      fonSize: fonSize,
+      fontWeight: FontWeight.bold,
+    );
+
+    //regular style
+TextStyle regularStyle({
+  Color color = AppColors.white,
+  double fonSize = 24,
+}) =>
+    _textStyle(
+      color: color,
+      fonSize: fonSize,
+      fontWeight: FontWeight.normal,
+    );
